0b7035745c7839746d3b5e06c938ee3995d3e987
PRABHAKAR9107 <02panjiyara@gmail.com>
2022-04-29 16:18:18 +0530

    add cards in project

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Card(props) {
+  return (
+    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img alt="robot" src={`https://robohash.org/${props.id}?300x300`} />
+      <div>
+        <h3> {props.name}</h3>
+        <p>{props.email}</p>
+      </div>
+    </div>
+  );
+}
