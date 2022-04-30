c3b3185f604a4a2bcc02bbd84190cd7238ca98d6
PRABHAKAR9107 <02panjiyara@gmail.com>
2022-05-01 03:49:50 +0530

    searchbar completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Cardlist from "./Cardlist";
 import { robots } from "./robots";
 
 import Searchbar from "./Searchbar";
 import "./App.css";
-export default function App() {
-  return (
-    <div className="tc">
-      <h1>Robofriends</h1>
 
-      <Searchbar />
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: "",
+    };
+  }
+  onsearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
 
-      <Cardlist robots={robots} />
-    </div>
-  );
+  render() {
+    const filteredRobots = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    return (
+      <div>
+        <div className="tc">
+          <h1>Robofriends</h1>
+
+          <Searchbar searchChange={this.onsearchChange} />
+
+          <Cardlist robots={filteredRobots} />
+        </div>
+      </div>
+    );
+  }
 }
--- src/Searchbar.js
+++ src/Searchbar.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-export default function Searchbar() {
+export default function Searchbar({ searchfield, searchChange }) {
   return (
     <div className="pa2">
       <input
         className="pa3 ba b--green  bg-lightest blue"
         type="search"
         placeholder="search robots"
+        onChange={searchChange}
       />
     </div>
   );
